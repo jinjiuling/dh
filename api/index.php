71a31472bdf154321f12a793618f2f094ce48992
jinjiuling <65697148+jinjiuling@users.noreply.github.com>
2021-08-02 18:39:47 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,9 +1,6 @@
 <?php 
 $id=$_GET['id'];
-include'gethtml.php';
-$html=gethtml($id);
-$rss =  @simplexml_load_string($html);
-$title=$rss->channel->title;
+$rss =  @simplexml_load_file($id);
 if ($rss == true) {
 preg_match_all ("/:\/\/(.*?)\//", $id, $n);
 $x=str_replace($n[0][0],"",$id);
@@ -11,13 +8,7 @@ $re=str_replace("/","",$x);
 $er=str_replace("http","",$re);
 $ter=$n[1][0].$er;
 $ters=str_replace(".","",$ter);
-//print_r($n);
-//echo $ters;
-$file=$ters.".txt";
-if(file_exists($file)){
-$d=file_get_contents($file);}
-else{echo "RSS已收录，再次请求开始计数";}
-
+$d=file_get_contents($ters.".txt");
 for ($i=0;$i<count($rss->channel->item);$i++){
 	$k.= strtotime($rss->channel->item[$i]->pubDate);
                $h=str_split($k,10);
@@ -26,17 +17,12 @@ $newArr = array();
 foreach($h as $vo){
 if($vo>$d)$newArr[] =$vo;	
 }
-//print_r($newArr);
-//echo $d;
 $l.=count($newArr);
-
-
 $data=array(data=>array(num=>$l));
 echo json_encode($data);
 $last= strtotime($rss->channel->item->pubDate);
 file_put_contents($ters.".txt",$last);
 } else {
 echo json_encode(array(data=>array(num=>-1)));
-//echo "-1";
 }
 ?>
