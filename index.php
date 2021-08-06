28160df8dcd1b3321173ec37cc9978530b400be8
jinjiuling <65697148+jinjiuling@users.noreply.github.com>
2021-08-06 16:43:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 require __DIR__ . '/gethtml.php';
 $rss=gethtml("https://rsshub.rssforever.com/dcard/sex");
-$rss1=str_replace("src=\"http","src=\"https://imgpro.eu-gb.cf.appdomain.cloud/2/index.php?q=http",$rss);
+$rss1=str_replace("src=\"http","src=\"https://jinrss.us-south.cf.appdomain.cloud/2/index.php?q=http",$rss);
 echo $rss1;
